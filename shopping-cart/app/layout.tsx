ca740d5e6801960f6b626f22e0517c78af2f7b3e
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-18 10:31:23 +0600

    shopping cart project basic structure done

--- shopping-cart/app/layout.tsx
+++ shopping-cart/app/layout.tsx
@@ -1,20 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+import { SiteHeader } from "@/components/site-header";
 
-const geistSans = Geist({
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  weight: "100 900",
 });
-
-const geistMono = Geist_Mono({
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "100 900",
 });
 
-export const metadata: Metadata = {
-  title: 'Ecommerce App',
-  description: 'A simple ecommerce application with Zustand',
+export const metadata = {
+  title: "Ecommerce App",
+  description: "A simple ecommerce application",
 };
 
 export default function RootLayout({
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SiteHeader />
         {children}
       </body>
     </html>
